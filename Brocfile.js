d0b1831cad6e4178ab74eaff5850729ec1f85872
ciembor <pub@ciemborowicz.pl>
2015-09-10 14:46:43 +0200

    Add html support.

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,13 @@
 var compileSass = require('broccoli-sass');
+var pickFiles = require('broccoli-static-compiler');
+var mergeTrees = require('broccoli-merge-trees');
+
 var scssDir = 'app/scss';
 var styles = compileSass([scssDir], 'application.scss', 'application.css');
-module.exports = styles;
+
+var html = pickFiles('app/html', {  
+  srcDir: '.',
+  destDir: '.'
+});
+
+module.exports = mergeTrees([styles, html]);  
